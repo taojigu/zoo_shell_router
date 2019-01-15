134b017951472fe6333104f2d3291d6b41866b0f
gujitao001 <gujitao001@ke.com>
2019-01-15 18:29:23 +0800

    change demo

---
+++ ios/Runner/GateViewController.h
@@ -0,0 +1,17 @@
+//
+//  GateViewController.h
+//  Runner
+//
+//  Created by lianjia on 2019/1/15.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GateViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Runner/GateViewController.m
@@ -0,0 +1,40 @@
+//
+//  GateViewController.m
+//  Runner
+//
+//  Created by lianjia on 2019/1/15.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+#import "GateViewController.h"
+#import <Flutter/Flutter.h>
+
+@interface GateViewController ()
+
+@end
+
+@implementation GateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)pushFlutterViewController:(id)sender {
+
+    FlutterViewController *flvc = [[FlutterViewController alloc] init];
+    [flvc setInitialRoute:@"tiger"];
+    [self.navigationController pushViewController:flvc animated:YES];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
